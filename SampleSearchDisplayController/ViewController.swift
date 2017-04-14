b47cd0e96d19a4a7b22429a80d3639a79bb1a418
Hitendra Mac <hitendra.spaceo@gmail.com>
2017-04-14 16:18:30 +0530

    Initial Commit

--- SampleSearchDisplayController/ViewController.swift
+++ SampleSearchDisplayController/ViewController.swift
@@ -8,18 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchResultsUpdating, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tblSearch: UITableView!
+    var searchController = UISearchController()
+
+    var arrCountry = ["Afghanistan", "Algeria", "Bahrain","Brazil", "Cuba", "Denmark","Denmark", "Georgia", "Hong Kong", "Iceland", "India", "Japan", "Kuwait", "Nepal"];
+    var arrFilter:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.searchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.searchBar.sizeToFit()
+            
+            //Add searchbar controller in header
+            self.tblSearch.tableHeaderView = controller.searchBar
+            
+            return controller
+        })()
+        
+        // Reload the table
+        self.tblSearch.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: UITableViewDelegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 44
+    }
+    
+    //MARK: UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (self.searchController.isActive) {
+            return self.arrFilter.count
+        } else {
+            return self.arrCountry.count
+        }
 
-
+    }
+   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        configureCell(cell: cell, forRowAtIndexPath: indexPath)
+        return cell
+    }
+    
+    
+    func configureCell(cell: UITableViewCell, forRowAtIndexPath: IndexPath) {
+        // 3
+        if (self.searchController.isActive) {
+            cell.textLabel?.text = arrFilter[forRowAtIndexPath.row]
+        } else {
+            cell.textLabel?.text = arrCountry[forRowAtIndexPath.row]
+        }
+    }
+    
+    //UISearch result update delegate
+    func updateSearchResults(for searchController: UISearchController) {
+        arrFilter.removeAll(keepingCapacity: false)
+        
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
+        let array = (arrCountry as NSArray).filtered(using: searchPredicate)
+        arrFilter = array as! [String]
+        
+        self.tblSearch.reloadData()
+    }
 }
 
