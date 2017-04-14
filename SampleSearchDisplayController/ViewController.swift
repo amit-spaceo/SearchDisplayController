48e1e148f58c06613a28b791470362bfee850849
Hitendra Mac <hitendra.spaceo@gmail.com>
2017-04-14 15:57:52 +0530

    Initial Commit

---
+++ SampleSearchDisplayController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSearchDisplayController
+//
+//  Created by Hitendra Mac on 14/04/17.
+//  Copyright © 2017 Spaceo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
